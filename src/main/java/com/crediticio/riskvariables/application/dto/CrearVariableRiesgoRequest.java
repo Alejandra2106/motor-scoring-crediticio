37ea9ececc1d10d0035731069b9b9377f17d2089
Alejandra2106 <malejandra.otalvaro1@udea.edu.co>
2026-09-20 15:17:36 -0500

    Documentar enum de variable de riesgo en Swagger.

--- src/main/java/com/crediticio/riskvariables/application/dto/CrearVariableRiesgoRequest.java
+++ src/main/java/com/crediticio/riskvariables/application/dto/CrearVariableRiesgoRequest.java
@@ -1,12 +1,18 @@
 package com.crediticio.riskvariables.application.dto;
 
 import com.crediticio.riskvariables.domain.NombreVariableRiesgo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class CrearVariableRiesgoRequest {
 
+    @Schema(description = "Variable de riesgo que se desea registrar. Corresponde a un catálogo cerrado de valores.",
+            example = "INGRESOS_MENSUALES",
+            requiredMode = Schema.RequiredMode.REQUIRED,
+            allowableValues = {"INGRESOS_MENSUALES", "NIVEL_ENDEUDAMIENTO", "NUMERO_MORAS", "HISTORIAL_CREDITICIO",
+                    "ANTIGUEDAD_LABORAL"})
     @NotNull(message = "variable es obligatoria")
     private NombreVariableRiesgo variable;
 
